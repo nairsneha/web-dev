b053aefe0c2cd60db2e886195d589ea967f91d2b
nairsneha <nair.sneha1997@gmail.com>
2022-02-28 00:18:51 -0500

    tuiter done finally added cyborg

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -1,6 +1,6 @@
-import NavigationSidebar from "../NavigationSidebar";
-import PostSummaryList from "../PostSummaryList";
-import PostItem from "../PostList";
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import PostSummaryList from "../PostSummaryList/index.js";
+import PostItem from "../PostList/index.js";
 
 (function ($) {
     $('#wd-explore').append(`
