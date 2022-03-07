d573f3532a85fceeedc81620011bf220877e0ed7
nairsneha <nair.sneha1997@gmail.com>
2022-03-07 16:40:19 -0500

    starting react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './vendors/bootstrap/css/bootstrap.min.css';
+
+import HelloWorld from "./components/HelloWorld";
+import Labs from "./components/Labs";
+import Tuiter from "./components/Tuiter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <HelloWorld/>
+          <Labs/>
+          <Tuiter/>
+
+      </div>
+
   );
 }
-
 export default App;
