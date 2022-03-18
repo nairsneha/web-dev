eb60e1628b687142a01e7753e3b0fa372049c408
nairsneha <nair.sneha1997@gmail.com>
2022-03-18 01:24:14 -0400

    starting a6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-
 import './vendors/bootstrap/css/bootstrap.min.css';
 
-import HelloWorld from "./components/HelloWorld";
-import Labs from "./components/Labs";
-import Tuiter from "./components/Tuiter";
-
 function App() {
-  return (
-      <div className="container">
-        <HelloWorld/>
-          <Labs/>
-          <Tuiter/>
-
-      </div>
-
-  );
+    return (
+        <h1>Hello World!</h1>
+    );
 }
 export default App;
+
+
