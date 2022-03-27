e4c9fc2757f293712eba8e69c1c42cd064cf79f9
nairsneha <nair.sneha1997@gmail.com>
2022-03-27 11:48:32 -0400

    adding redirects file

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReduxExamples from "./redux-examples";
+import {Link} from "react-router-dom";
 
 const Labs = () => {
     return(
         <>
             <h1>Labs</h1>
+            <Link to="/hello">
+                Hello
+            </Link> |
+            <Link to="/tuiter">
+                Tuiter
+            </Link>
             <ReduxExamples/>
         </>)
 };
